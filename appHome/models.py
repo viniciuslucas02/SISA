412a106f9b8e38fb153774ca6559125a1ead177b
Fabio <fabiojosedelima011@gmail.com>
2025-11-05 21:45:07 -0300

    Cadastrando no banco

--- appHome/models.py
+++ appHome/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=255)
+    faculdade = models.CharField(max_length=255)
+    curso = models.CharField(max_length=255)
+    turno = models.CharField(max_length=20)
+    emitido = models.DateField()
+
